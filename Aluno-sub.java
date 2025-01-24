654e939f1f2a59bae399e3555721879c1a703580
pedrokekeww <pedroveb119@gmail.com>
2025-01-24 14:02:48 -0300

    Create Aluno-sub.java

---
+++ Aluno-sub.java
@@ -0,0 +1,38 @@
+package poo.p3;
+
+public class Aluno extends Pessoa{
+    private String curso;
+	private int periodo;
+	
+	public Aluno(double idade, int periodo) {
+		super(idade); 
+		System.out.println("Esse aluno ta no P" + periodo + " e é um botzin");
+	}
+	public Aluno() {
+		super();
+		System.out.println("Esse aluno ta matriculado formalmente no campus.");
+	}
+	public String getCurso() 
+	{
+		return curso;
+	}
+	public void setCurso(String curso) 
+	{
+		this.curso = curso;
+	}
+	public int getPeriodo() 
+	{
+		return periodo;
+	}
+	public void setPeriodo(int periodo) 
+	{
+		this.periodo = periodo;
+	}
+	
+	public String infDoCurso(String infDoCurso) {
+		return "Esse aluno cursa " + curso + ": " + infDoCurso;
+	}
+	public String periodosleft() {
+		return "Falta " + (6-periodo) + " periodos para esse aluno finalizar seu curso.";
+	}
+}
